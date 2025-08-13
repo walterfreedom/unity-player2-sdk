85484abadc010d02dcd1a35ce1a2ff4818181e67
Edvin Bryntesson <epost@edvinbryntesson.se>
2025-08-13 16:01:22 +0200

    remove unnecessary comments

--- ExampleFunctionHandler.cs
+++ ExampleFunctionHandler.cs
@@ -16,18 +16,6 @@ namespace player2_sdk
                 Debug.Log($"Argument: {arg.Key} = {arg.Value}");
             }
         
-            //optional: use npcObject to access the name of the NPC that called the function. prints the name of the gameobject.
-            // try
-            // {
-            //    Debug.Log(functionCall.aiObject.name);
-            // }
-            // catch (Exception e)
-            // {
-            //     Console.WriteLine(e);
-            //     throw;
-            // }
-        
-
             // Here you would implement your actual function logic
             // For example, if this is a chat function, you might send a message to the NPC
         }
